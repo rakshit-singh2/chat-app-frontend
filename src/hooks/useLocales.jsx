458f9fa689df52d81f35a53731866a58382c03c0
rakshit-singh2 <thisisjames007@gmail.com>
2024-10-10 16:16:47 +0530

    useLocales

--- src/hooks/useLocales.jsx
+++ src/hooks/useLocales.jsx
@@ -1,17 +1,39 @@
-import { useTranslation } from 'react-i18next';
+// src/hooks/useLocales.js
+import i18n from 'i18next';
+import { initReactI18next, useTranslation } from 'react-i18next';
 import useSettings from './useSettings';
-// config
 import { allLangs, defaultLang } from '../config';
 
-// ----------------------------------------------------------------------
+// Initialize i18next
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: {
+          welcome: "Welcome",
+          
+        },
+      },
+      fr: {
+        translation: {
+          welcome: "Bienvenue",
+          
+        },
+      },
+    },
+    lng: localStorage.getItem('i18nextLng') || defaultLang.value,
+    fallbackLng: defaultLang.value,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 export default function useLocales() {
-  const { i18n, t: translate } = useTranslation();
-
+  const { t: translate } = useTranslation(); // Use the i18n instance here
   const { onChangeDirectionByLang } = useSettings();
 
   const langStorage = localStorage.getItem('i18nextLng');
-
   const currentLang = allLangs.find((_lang) => _lang.value === langStorage) || defaultLang;
 
   const handleChangeLanguage = (newlang) => {
