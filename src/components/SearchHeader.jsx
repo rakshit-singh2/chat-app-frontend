e657c5d1f4bc0d200ef231ba461b506a89a755d5
rakshit-singh1 <thisisjames007@gmail.com>
2025-01-13 15:10:40 +0530

    friends

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,31 +1,48 @@
 
 import { Stack, Typography, IconButton, InputBase, Button, Divider } from "@mui/material";
-import { CircleDashed, MagnifyingGlass, ArchiveBox } from 'phosphor-react';
+import { CircleDashed, Users, MagnifyingGlass, ArchiveBox } from 'phosphor-react';
 import { useTheme } from '@mui/material/styles';
 import Search from "./Search/Search";
 import SearchIconWrapper from "./Search/SearchIconWrapper";
 import StyledInputBase from "./Search/StyledInputBase";
+import { useState } from "react";
+import Friends from "../sections/main/Friends";
 
-const SearchHeader = ({header, }) => {
+const SearchHeader = ({ header, }) => {
+    const [openDialogueBox, setOpenDialogueBox] = useState(false);
 
     const theme = useTheme();
 
-  return (
-    <>
-        <Stack 
+    const handleCloseDialogue = () => {
+        setOpenDialogueBox(false);
+    }
+    const handleOpenDialogue = () => {
+        setOpenDialogueBox(true);
+    }
+
+    return (
+        <>
+            <Stack
                 direction={"row"}
                 alignItems={"center"}
                 justifyContent={"space-between"}
             >
-                <Typography 
+                <Typography
                     variant="h5"
                     letterSpacing={1}
                 >
                     {header}
                 </Typography>
-                <IconButton>
-                    <CircleDashed size={32} />
-                </IconButton>
+                <Stack direction="row" alignItems="center" spacing={1}>
+                    <IconButton onClick={() => {
+                        handleOpenDialogue();
+                    }}>
+                        <Users size={32} />
+                    </IconButton>
+                    <IconButton>
+                        <CircleDashed size={32} />
+                    </IconButton>
+                </Stack>
             </Stack>
             <Stack
                 sx={{
@@ -34,16 +51,17 @@ const SearchHeader = ({header, }) => {
             >
                 <Search>
                     <SearchIconWrapper>
-                        <MagnifyingGlass color="#709CE6" size={32}/>
+                        <MagnifyingGlass color="#709CE6" size={32} />
                     </SearchIconWrapper>
                     <StyledInputBase
                         placeholder="Search…"
                         inputProps={{ 'aria-label': 'search' }}
                     />
                 </Search>
-            </Stack>       
-    </>
-  )
+            </Stack>
+            {openDialogueBox && <Friends open={openDialogueBox} handleClose={handleCloseDialogue} />}
+        </>
+    )
 }
 
 export default SearchHeader;
--- src/pages/dashboard/Chats.jsx
+++ src/pages/dashboard/Chats.jsx
@@ -51,6 +51,8 @@ const Chats = () => {
         </Stack>
       </Stack>
     </Box>
+
+    
   )
 }
 
